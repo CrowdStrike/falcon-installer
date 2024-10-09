1508c1a2b66927aa767cd4c86546715c213b2b41
Gabe Alford <redhatrises@gmail.com>
2024-10-09 12:18:25 -0600

    fix: provide systemctl cmnd through PATH and return err if in local dir

--- pkg/systemd/systemd.go
+++ pkg/systemd/systemd.go
@@ -7,12 +7,12 @@ import (
 	"github.com/crowdstrike/falcon-installer/pkg/utils"
 )
 
-const systemctlCmd = "/usr/bin/systemctl"
+var systemctlCmd = "systemctl"
 
 // RestartService restarts a systemd service
 func RestartService(name string) error {
-	if _, err := exec.LookPath(systemctlCmd); err != nil {
-		return fmt.Errorf("Could not find systemctl: %s", systemctlCmd)
+	if systemctlCmd, err := exec.LookPath(systemctlCmd); err != nil {
+		return fmt.Errorf("Could not find systemctl: %s, %v", systemctlCmd, err)
 	}
 
 	args := []string{"restart", name}
