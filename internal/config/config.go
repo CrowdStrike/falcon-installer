7e0922d8e5b3e2a2436640c490eb5f298a414092
schwitm <matthias.schwitzgebel@mercedes-benz.com>
2025-09-24 08:19:52 +0000

    Added support for Azure user managed identities

--- internal/config/config.go
+++ internal/config/config.go
@@ -25,6 +25,8 @@ package config
 import (
 	"fmt"
 
+	"os"
+
 	"github.com/crowdstrike/falcon-installer/internal/vault"
 	"github.com/crowdstrike/falcon-installer/pkg/installer"
 	"github.com/spf13/viper"
@@ -41,6 +43,7 @@ func Load() (*Config, error) {
 	ociVault := viper.GetString("oci_vault_name")
 	ociCompartmentID := viper.GetString("oci_compartment_id")
 	azureVault := viper.GetString("azure_vault_name")
+	managedIdentity := viper.GetString("azure_managed_identity")
 	awsSecret := viper.GetString("aws_secret_name")
 	awsRegion := viper.GetString("aws_secret_region")
 	gcpVault := viper.GetString("gcp_project_id")
@@ -50,7 +53,12 @@ func Load() (*Config, error) {
 	case awsSecret != "" && awsRegion != "":
 		secrets, err = vault.GetAWSSecretsManagerSecrets(awsRegion, awsSecret)
 	case azureVault != "":
-		secrets, err = vault.GetAzureKeyVaultSecrets(azureVault)
+		{
+			if managedIdentity != "" {
+				os.Setenv("AZURE_CLIENT_ID", managedIdentity)
+			}
+			secrets, err = vault.GetAzureKeyVaultSecrets(azureVault)
+		}
 	case gcpVault != "":
 		secrets, err = vault.GetGCPSecretManagerSecrets(gcpVault)
 	case ociVault != "" && ociCompartmentID != "":
